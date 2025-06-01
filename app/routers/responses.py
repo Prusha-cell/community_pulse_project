8945870a4d1af9074c764670985169d9d14fa1bb
ICH_pycharm_bash <anubis2016prusha@gmail.com>
2025-06-01 22:14:50 +0200

    Initial commit

---
+++ app/routers/responses.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+
+response_bp = Blueprint('response', __name__, url_prefix='/responses')
+
+@response_bp.route('/', methods=['GET'])
+def get_responses():
+    """Получение статистики ответов."""
+    return "Статистика всех ответов"
+
+@response_bp.route('/', methods=['POST'])
+def add_response():
+    """Добавление нового ответа на вопрос."""
+    return "Ответ добавлен"
